2e23af9f45034a158d33ba8832f2834fdd43f8a4
Alex Stek <astek98@gmail.com>
2023-10-14 00:27:49 -0400

    capi-byoh wip1

--- cloud-init-capi.sh
+++ cloud-init-capi.sh
@@ -15,28 +15,29 @@ VERSION_KUBECTL="v1.26.9"
 # VERSION_KUBECTL=(curl -L -s https://dl.k8s.io/release/stable.txt)
 curl -LO "https://dl.k8s.io/release/${VERSION_KUBECTL}/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-kubectl version -o json
+# kubectl version -o json
 
 echo "# Install clusterctl..."
 VERSION_CLUSTERCTL="v1.5.2"
 curl -o clusterctl -L https://github.com/kubernetes-sigs/cluster-api/releases/download/${VERSION_CLUSTERCTL}/clusterctl-linux-amd64
 sudo install -o root -g root -m 0755 clusterctl /usr/local/bin/clusterctl
-clusterctl version -o json
+# clusterctl version -o json
 
 # Maybe will require to change to package installatiom
 echo "# Install docker by script..."
-curl -fsSL https://get.docker.com -o get-docker.sh | sudo sh get-docker.sh
-docker version
+curl -fsSL https://get.docker.com -o get-docker.sh 
+sudo sh get-docker.sh
+# docker version
 
 echo "# Install kind..."
 VERSION_KIND="v0.20.0"
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/${VERSION_KIND}/kind-linux-amd64
 sudo install -o root -g root -m 0755 kind /usr/local/bin/kind
-kind version
+# kind version
 
 echo "# Install helm..."
 curl -sL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | sudo bash
-helm version
+# helm version
 
 echo "# complete!"
 

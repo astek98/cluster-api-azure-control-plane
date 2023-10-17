1262a12e2c4241352d67c43cb06fdf014352e9fd
Alex Stek <astek98@gmail.com>
2023-10-16 22:31:11 -0400

    update with curl

--- cloud-init-capi.sh
+++ cloud-init-capi.sh
@@ -13,30 +13,31 @@ echo "# Installing prerequisites ..."
 echo "# Install kubectl..."
 VERSION_KUBECTL="v1.26.9"
 # VERSION_KUBECTL=(curl -L -s https://dl.k8s.io/release/stable.txt)
-curl -LO "https://dl.k8s.io/release/${VERSION_KUBECTL}/bin/linux/amd64/kubectl"
+curl  -o kubectl -fsSL "https://dl.k8s.io/release/${VERSION_KUBECTL}/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 # kubectl version -o json
 
 echo "# Install clusterctl..."
 VERSION_CLUSTERCTL="v1.5.2"
-curl -o clusterctl -L https://github.com/kubernetes-sigs/cluster-api/releases/download/${VERSION_CLUSTERCTL}/clusterctl-linux-amd64
+curl -o clusterctl -fsSL https://github.com/kubernetes-sigs/cluster-api/releases/download/${VERSION_CLUSTERCTL}/clusterctl-linux-amd64
 sudo install -o root -g root -m 0755 clusterctl /usr/local/bin/clusterctl
 # clusterctl version -o json
 
 # Maybe will require to change to package installatiom
 echo "# Install docker by script..."
-curl -fsSL https://get.docker.com -o get-docker.sh 
+curl -o get-docker.sh -fsSL https://get.docker.com
 sudo sh get-docker.sh
 # docker version
 
 echo "# Install kind..."
 VERSION_KIND="v0.20.0"
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/${VERSION_KIND}/kind-linux-amd64
+curl -o kind -fsSL https://kind.sigs.k8s.io/dl/${VERSION_KIND}/kind-linux-amd64
 sudo install -o root -g root -m 0755 kind /usr/local/bin/kind
 # kind version
 
 echo "# Install helm..."
-curl -sL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | sudo bash
+curl -o get-helm-3.sh -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+sudo sh get-helm-3.sh
 # helm version
 
 echo "# complete!"

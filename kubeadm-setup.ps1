de19e8d39ee3efa32d8f07d8f9e89f6996f1ffa1
Alex Stek <astek98@gmail.com>
2023-10-22 18:11:50 -0400

    WIP4

--- kubeadm-setup.ps1
+++ kubeadm-setup.ps1
@@ -1,6 +1,6 @@
 az account set --subscription ea23f33b-f226-4c3f-9c14-aabb8b63b8c8
 az group create -l eastus -n k0s-rgp-001
-$grp_name="k0smotron-rgp"
+$grp_name="kde-rgp"
 az group create -l eastus -n $grp_name
 
 az vm create `
@@ -13,6 +13,14 @@ az vm create `
 --nic-delete-option Delete `
 --% --public-ip-address ""
 
+az vm create `
+--name kde `
+--resource-group  $grp_name `
+--image Ubuntu2204 `
+--admin-username azureuser --admin-password Geico@12345! `
+--os-disk-delete-option Delete `
+--nic-delete-option Delete `
+--custom-data cloud-config-kde-rdp.yml
 
 az vm boot-diagnostics enable --ids $(az vm list -g $grp_name --query "[].id" -o tsv)
 az vm auto-shutdown --ids $(az vm list -g $grp_name --query "[].id" -o tsv) --time 2300
@@ -26,4 +34,4 @@ az vm delete --ids $(az vm list -g $grp_name --query "[].id" -o tsv) --no-wait -
 
 az vm show -g $grp_name -n vm-node-0 -d -u
 
-az vm open-port --resource-group $grp_name --name kmono --port 3389
+az vm open-port --resource-group $grp_name --name kde --port 3389
